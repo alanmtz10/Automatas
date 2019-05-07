23be56e157116e0a929a9a0dff8494576d60c92c
Alan <bronkbsgck@gmail.com>
2019-05-07 14:09:48 -0500

    Tabla predictiva terminada}

--- src/programa/Pruebas.java
+++ src/programa/Pruebas.java
@@ -15,15 +15,30 @@ import lexema.Lexema;
  */
 public class Pruebas {
 
+    public static String[] prbs;
+
     public static void main(String[] args) {
-        ArrayList<String> lineas = separarPalabras.SeparaPalabras.leer("programa2.txt");
-        ArrayList<Lexema> palabras = separarPalabras.SeparaPalabras.separa(lineas);
-        
-        for (Lexema palabra : palabras) {
-            System.out.println(palabra);
+        getPalabras();
+        for (String arg : prbs) {
+            System.out.println(arg);
         }
+    }
 
+    public static String[] getPalabras() {
+        ArrayList<String> lineas = separarPalabras.SeparaPalabras.leer("pro.txt");
+        ArrayList<Lexema> palabras = separarPalabras.SeparaPalabras.separa(lineas);
+
+        prbs = new String[palabras.size()+2];
+
+        for (int i = 0; i < palabras.size(); i++) {
+            prbs[i] = palabras.get(i).getLexema().trim();
+        }
+        
+        prbs[prbs.length-2] = "}";
+        prbs[prbs.length-1] = "$";
+        
         
 
+        return prbs;
     }
 }
