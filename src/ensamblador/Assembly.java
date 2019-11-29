f2e95897c62236500c633aeb6efbaf4951105a15
alanmtz10 <bronkbsgck@gmail.com>
2019-11-28 23:41:13 -0600

    Suma,resta y multiplicacion en ensamblador - 1 digito

--- src/ensamblador/Assembly.java
+++ src/ensamblador/Assembly.java
@@ -28,21 +28,55 @@ public class Assembly {
             + "\nADD AX,CX"
             + "\nMOV :res, AX";
 
+    public static final String resta = "MOV AX, :var1"
+            + "\nMOV CX, :var2"
+            + "\nSUB AX,CX"
+            + "\nMOV :res, AX";
+
+    public static final String multiplicacion = "MOV AX, :var1"
+            + "\nMOV CX, :var2"
+            + "\nMUL CX"
+            + "\nMOV :res, AX";
+
+    public static final String convertirAscii = "MOV AX,:var"
+            + "\nADD AX,48"
+            + "\nMOV :var,AX";
+
+    public static final String imprNum = "MOV DX,:var"
+            + "\nMOV AH,2"
+            + "\nINT 21h";
+
     public static void toAssembly(ArrayList<Cuadrupla> cuadruplas) {
         String aux;
-        System.out.println(encabezado);
+        System.out.println(encabezado + "\n");
         for (int i = 0; i < cuadruplas.size(); i++) {
             System.out.println(defWord.replace(":var", cuadruplas.get(i).getResultado().getLexema()));
         }
-        System.out.println(codigo);
+        System.out.println("\n" + codigo + "\n");
         for (int i = 0; i < cuadruplas.size(); i++) {
             if (cuadruplas.get(i).getOperacion().getLexema().equals("+")) {
                 aux = suma.replace(":var1", cuadruplas.get(i).getOperando1().getLexema());
                 aux = aux.replace(":var2", cuadruplas.get(i).getOperando2().getLexema());
                 aux = aux.replace(":res", cuadruplas.get(i).getResultado().getLexema());
-                System.out.println(aux);
+                System.out.println(aux + "\n");
+            } else if (cuadruplas.get(i).getOperacion().getLexema().equals("-")) {
+                aux = resta.replace(":var1", cuadruplas.get(i).getOperando1().getLexema());
+                aux = aux.replace(":var2", cuadruplas.get(i).getOperando2().getLexema());
+                aux = aux.replace(":res", cuadruplas.get(i).getResultado().getLexema());
+                System.out.println(aux + "\n");
+            } else if (cuadruplas.get(i).getOperacion().getLexema().equals("*")) {
+                aux = multiplicacion.replace(":var1", cuadruplas.get(i).getOperando1().getLexema());
+                aux = aux.replace(":var2", cuadruplas.get(i).getOperando2().getLexema());
+                aux = aux.replace(":res", cuadruplas.get(i).getResultado().getLexema());
+                System.out.println(aux + "\n");
             }
         }
+        String convA = convertirAscii.replace(":var", cuadruplas.get(cuadruplas.size() - 1).getResultado().getLexema());
+        System.out.println(convA + "\n");
+
+        String imp = imprNum.replace(":var", cuadruplas.get(cuadruplas.size() - 1).getResultado().getLexema());
+        System.out.println(imp + "\n");
+
         System.out.println(fin);
 
     }
